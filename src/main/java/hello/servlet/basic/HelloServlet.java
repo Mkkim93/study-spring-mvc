fe9308d6dbf557cb50eeef59d1bfabd5d6d7fb2b
mkkim <king0031443@gmail.com>
2024-05-22 13:06:25 +0900

    - feat : spring mvc study

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username"); // 쿼리 파라미터값 조회
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); // HTML <Head> 내, contentType 입력
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println("Hello " + username); // HTML <Body> 내 데이터(username) 입력
+    }
+}
